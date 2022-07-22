20f50890e707d46c3f243a5bdcf25f9c950a6ed5
Yohanes <yohanesabing@gmail.com>
2022-07-22 21:42:38 +0700

    Change dropdown menu to side menu drawer

--- app/src/main/java/com/example/cafelegend/HomeActivity.java
+++ app/src/main/java/com/example/cafelegend/HomeActivity.java
@@ -10,21 +10,44 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navView;
+    Toolbar toolbar;
     TextView welcomeMessageTV;
     Bundle extras;
+    String username;
 
     void init(){
         welcomeMessageTV = findViewById(R.id.welcome_TV);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navView = findViewById(R.id.navView);
+        toolbar = findViewById(R.id.toolbar);
+    }
+
+    void setupDrawer(){
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open_drawer, R.string.close_drawer);
+
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+        navView.setNavigationItemSelectedListener(this);
     }
 
     @SuppressLint("SetTextI18n")
     void setWelcomeMessage(){
         extras = getIntent().getExtras();
-        welcomeMessageTV.setText("Welcome, "+ extras.getString("username"));
+        username = extras.getString("username");
+        welcomeMessageTV.setText("Welcome, "+ username);
     }
 
     @Override
@@ -34,23 +57,24 @@ public class HomeActivity extends AppCompatActivity {
 
         init();
         setWelcomeMessage();
+        setupDrawer();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_home, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu_home, menu);
+//        return true;
+//    }
 
-    @SuppressLint("NonConstantResourceId")
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         Intent intent;
 
         switch(item.getItemId()){
             case R.id.nav_home:
                 intent = new Intent(this, HomeActivity.class);
+                intent.putExtra("username", username);
                 startActivity(intent);
                 finish();
                 break;
@@ -74,7 +98,6 @@ public class HomeActivity extends AppCompatActivity {
                 break;
             default:
         }
-
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 }
