76589c23bbde7d8eb9bbba6f7c3fd2af97eb6a78
Yohanes <yohanesabing@gmail.com>
2022-08-05 17:05:19 +0700

    Add logic in detail page

--- app/src/main/java/com/example/cafelegend/DetailActivity.java
+++ app/src/main/java/com/example/cafelegend/DetailActivity.java
@@ -1,9 +1,15 @@
 package com.example.cafelegend;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -20,13 +26,16 @@ public class DetailActivity extends AppCompatActivity {
 
     ImageView foodImage_IV;
     TextView foodName_TV, foodPrice_TV, foodDesc_TV;
+    EditText quantity_ET;
+    Button order_btn, back_btn;
 
     Bundle extras;
-    String foodName, foodDesc;
+    String username, foodName, foodDesc;
     int foodPrice, foodImage;
 
     void init(){
         extras = getIntent().getExtras();
+        username = extras.getString("username");
         foodName =  extras.getString("foodName");
         foodPrice =  extras.getInt("foodPrice");
         foodDesc =  extras.getString("foodDesc");
@@ -36,12 +45,65 @@ public class DetailActivity extends AppCompatActivity {
         foodPrice_TV = findViewById(R.id.foodPrice_TV);
         foodDesc_TV = findViewById(R.id.foodDesc_TV);
         foodImage_IV = findViewById(R.id.detail_image);
+        quantity_ET = findViewById(R.id.quantity_ET);
+        order_btn = findViewById(R.id.order_btn);
+        back_btn = findViewById(R.id.back_btn);
 
         foodImage_IV.setImageDrawable(this.getDrawable(foodImage));
         foodName_TV.setText(foodName);
         foodPrice_TV.setText("Rp. " + String.valueOf(foodPrice) + ",-");
         foodDesc_TV.setText(foodDesc);
 
+        setEventListener();
+    }
+
+    void setEventListener(){
+        setOrderEvent();
+        setBackEvent();
+    }
+
+    void setOrderEvent(){
+        order_btn.setOnClickListener(x -> {
+            String quantity = quantity_ET.getText().toString();
+            if(quantity.isEmpty()){
+                createDialog();
+                return;
+            }
+
+            Intent intent = new Intent(this, ItemsActivity.class);
+            intent.putExtra("username", username);
+            Toast.makeText(getApplicationContext(),
+                    "Order Successful",
+                    Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+    }
+
+    void setBackEvent(){
+        back_btn.setOnClickListener(x -> {
+            onBackPressed();
+        });
+    }
+
+    void createDialog(){
+        // Create the object of AlertDialog Builder class
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        // Set the message
+        builder.setMessage("Quantity can not be empty");
+
+        // Set Cancelable false
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
     @Override
@@ -50,12 +112,6 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         init();
-
-        System.out.println(foodName);
-        System.out.println(foodPrice);
-        System.out.println(foodDesc);
-        System.out.println(foodImage);
-
     }
 
 }
