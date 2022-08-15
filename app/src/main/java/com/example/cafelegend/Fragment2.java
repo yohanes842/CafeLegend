c180d05dfd825aed0a5ff9e61547279fbe17f759
vannessa21 <vannessaaugustine21@gmail.com>
2022-08-15 21:34:37 +0700

    Menu items done

--- app/src/main/java/com/example/cafelegend/Fragment2.java
+++ app/src/main/java/com/example/cafelegend/Fragment2.java
@@ -1,64 +1,65 @@
 package com.example.cafelegend;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.cafelegend.adapter.ItemsAdapter;
+import com.example.cafelegend.model.Food;
+
+import java.util.Vector;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Fragment2#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Fragment2 extends Fragment {
+public class Fragment2 extends Fragment implements ItemsAdapter.OnEventListener{
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    View v;
+    private Vector<Food> foodVector;
 
     public Fragment2() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment2.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment2 newInstance(String param1, String param2) {
-        Fragment2 fragment = new Fragment2();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        foodVector = new Vector<>();
+        foodVector.add(new Food("Grilled Chicken With Fresh Cherry Salsa", "Juicy grilled chicken breasts, topped with a delicious ruby-red cherry salsa, made from the season’s best local cherries. Quick, easy and super yummy, this the ultimate summer plate!", 49000, R.drawable.grilled_chicken));
+        foodVector.add(new Food("Roasted Portobello With Shrimp", "Kick Off The Spring/Summer With This Delicious Roasted Portobello Mushroom With A Light, Tangy And Slightly Spicy Shrimp ‘N Tomato Bruschetta Topping. It Doesn’t Taste Any Fresher Than This!", 58000, R.drawable.roasted_protobello));
+        foodVector.add(new Food("Almond Crusted Chicken", "A gluten free and healthy chicken dinner with maple dijon sauce that will become weeknight favorite!", 61000, R.drawable.almond_crusted_chicken));
+        foodVector.add(new Food("Beef Goulash", "Beef Goulash is essentially a hearty beef stew seasoned with paprika and other spices, that originated in Hungary.", 57000, R.drawable.beef_goulash));
+        foodVector.add(new Food("Crispy Belly", "Roasted crispy pork belly, served with mango salad and thai chili sauce.", 48000, R.drawable.crispy_belly));
+        foodVector.add(new Food("Truffle Cream Wild Mushroom", "Creamy Fettuccine, Grated Parmesan. ", 55000, R.drawable.truffle));
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        v= inflater.inflate(R.layout.fragment_2, container, false);
+        RecyclerView rv = v.findViewById(R.id.menu_food);
+        ItemsAdapter foodAdapter = new ItemsAdapter(getContext(), foodVector, this);
+        rv.setAdapter(foodAdapter);
+        rv.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        return v;
+    }
+
+    @Override
+    public void onCardClick(Vector<Food> foodVector, int position) {
+        Intent intent = new Intent(getContext(), DetailActivity.class);
+
+        intent.putExtra("food", foodVector.get(position));
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/cafelegend/Fragment3.java
+++ app/src/main/java/com/example/cafelegend/Fragment3.java
@@ -1,64 +1,67 @@
 package com.example.cafelegend;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.cafelegend.adapter.ItemsAdapter;
+import com.example.cafelegend.model.Food;
+
+import java.util.Vector;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Fragment3#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Fragment3 extends Fragment {
+public class Fragment3 extends Fragment implements ItemsAdapter.OnEventListener{
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    View v;
+    private Vector<Food> foodVector;
 
     public Fragment3() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment3.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment3 newInstance(String param1, String param2) {
-        Fragment3 fragment = new Fragment3();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        foodVector = new Vector<>();
+        foodVector.add(new Food("Chocolate Frappucino", "Mocha sauce and Frappuccino chips are blended with milk and ice, layered on top of whipped cream and chocolate cookie crumble and topped with vanilla whipped cream, mocha drizzle and even more chocolate cookie crumble. These layers ensure each sip is as good as the last; all the way to the end.", 35000, R.drawable.chocolate_frappucinno));
+        foodVector.add(new Food("Earl Grey Tea", "We take a strong black tea base and add the essence of bergamot, a citrus fruit with subtle lemon and floral lavender notes, to create this aromatically awesome tea flavor.", 18000, R.drawable.earl_grey_tea));
+        foodVector.add(new Food("Hot Chocolate", "Steamed milk and mocha sauce topped with sweetened whipped cream and a chocolate-flavored drizzle. A timeless classic made to sweeten your spirits.", 25000, R.drawable.hot_choco));
+        foodVector.add(new Food("Iced Americano", "Freshly brewed Iced Coffee Blend served chilled and sweetened over ice. An absolutely, seriously, refreshingly lift to any day.", 23000, R.drawable.iced_americano));
+        foodVector.add(new Food("Iced caramel Macchiato", "We combine our rich, full-bodied espresso with vanilla-flavored syrup, milk and ice, then top it off with a caramel drizzle for an oh-so-sweet finish.", 35000, R.drawable.iced_caramel_macchiato));
+        foodVector.add(new Food("Iced Latte", "Small-lot espresso combined with milk and served over ice creates perfectly handcrafted sips of cool.", 23000, R.drawable.iced_latte));
+        foodVector.add(new Food("Kiwi-StarFruit Refreshers", "Starfruit- and kiwi-flavored juice and real kiwi fruit pieces shaken with ice. Deliciously refreshing.", 27000, R.drawable.kiwi_starfruit));
+        foodVector.add(new Food("Pumpkin Spice Latte", "Our signature espresso and steamed milk with the celebrated flavor combination of pumpkin, cinnamon, nutmeg and clove. Enjoy it topped with whipped cream and real pumpkin-pie spices.", 35000, R.drawable.pumpkin_spice_latte));
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_3, container, false);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        v= inflater.inflate(R.layout.fragment_3, container, false);
+        RecyclerView rv = v.findViewById(R.id.menu_food);
+        ItemsAdapter foodAdapter = new ItemsAdapter(getContext(), foodVector, this);
+        rv.setAdapter(foodAdapter);
+        rv.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        return v;
+    }
+
+    @Override
+    public void onCardClick(Vector<Food> foodVector, int position) {
+        Intent intent = new Intent(getContext(), DetailActivity.class);
+
+        intent.putExtra("food", foodVector.get(position));
+        startActivity(intent);
     }
 }
